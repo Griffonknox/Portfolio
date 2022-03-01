032a5961a7c27dd35cd47b301dfefbde23d08d27
Scott Thompson <thompsonscott1180@gmail.com>
2022-03-01 13:40:34 -0600

    LatestUpdate

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,7 +3,14 @@ from flask import render_template
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("home.html")
+    return render_template("home.html", nav_type="about_me")
 
 
+@bp.route('/projects', methods=['GET', 'POST'])
+def projects():
+    return render_template("projects.html", nav_type="projects")
 
+
+@bp.route('/work_experience', methods=['GET', 'POST'])
+def work_experience():
+    return render_template("work_experience.html", nav_type="work_experience")
