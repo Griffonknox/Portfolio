4bc7fda9cc10906cd31d52b74853b570e15e54ca
griffonknox <thompsonscott1180@gmail.com>
2022-03-01 17:33:35 -0600

    MobileModifications

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 class Config(object):
     SECRET_KEY = 'a5f7f3f3d18eaa317ac1e36a5632a862a98ace1c97dd4ac3'
 
-    DEBUG = False
+    DEBUG = True
     POSTS_PER_PAGE = 5
 
     GIT_URL = "https://github.com/Griffonknox"
