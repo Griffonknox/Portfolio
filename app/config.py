b94520789afa2a7da7708346d5ba436cda68ee40
Scott Thompson <thompsonscott1180@gmail.com>
2022-03-01 13:45:45 -0600

    Config.py

---
+++ app/config.py
@@ -0,0 +1,8 @@
+class Config(object):
+    SECRET_KEY = 'a5f7f3f3d18eaa317ac1e36a5632a862a98ace1c97dd4ac3'
+
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = True
+    POSTS_PER_PAGE = 5
+
