bd94c4bc84b2a18b1aad6e2b1a39cb96d84f56b3
Scott Thompson <thompsonscott1180@gmail.com>
2022-03-29 09:18:09 -0500

    Resume; Projects

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
 from app.main import bp
-from flask import render_template
+from flask import render_template, make_response, url_for, redirect
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,3 +14,12 @@ def portfolio():
 @bp.route('/work_experience', methods=['GET', 'POST'])
 def work_experience():
     return render_template("work_experience.html", nav_type="work_experience")
+
+@bp.route('/resume', methods=["GET", "POST"])
+def resume():
+    # binary_pdf = get_binary_pdf_data_from_database(id=id)
+    # response = make_response(binary_pdf)
+    # response.headers['Content-Type'] = 'application/pdf'
+    # response.headers['Content-Disposition'] = 'inline; filename=%ThompsonScott.pdf'
+    # return response
+    return redirect(url_for('static', filename='resume.pdf'))
